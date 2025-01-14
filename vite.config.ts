e3ec1d6c713cd96ca45c2504ccda2f5a068e2391
Coltos Iohann <coltosiohann@gmail.com>
2025-01-14 16:31:01 +0200

    update deployment 2 root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yusuf-farm/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
     
